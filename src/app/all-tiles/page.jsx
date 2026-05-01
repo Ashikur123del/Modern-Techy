66c105e05be2846f64eb641f757b83f26aa8f09f
ashik <leadskillit@gmail.com>
2026-05-01 14:38:16 +0600

    update

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,21 +1,26 @@
 import AllTilesHero from "@/components/AllTilesHero";
-import Card from "@/components/Card"; 
+import Card from "@/components/Card";
 
 const AllTiles = async () => {
-  const res = await fetch('https://modern-techy.vercel.app/db.json', { cache: 'no-store'});
-  const allTiles = await res.json(); 
-  
+  const res = await fetch('https://modern-techy.vercel.app/db.json', {
+    cache: "no-store"
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const allTiles = await res.json();
+
   return (
     <div className="container mx-auto p-4">
       <AllTilesHero />
-      
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
-        {
-          allTiles?.map((tile) => (
-           
-            <Card key={tile.id} item={tile}  />
-          ))
-        }
+        {Array.isArray(allTiles) &&
+          allTiles.map((tile) => (
+            <Card key={tile.id} item={tile} />
+          ))}
       </div>
     </div>
   );
